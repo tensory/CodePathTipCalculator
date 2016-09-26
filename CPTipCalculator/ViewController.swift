2a07dc750141ecdb38566310eb0f78055a52101a
Ari Lacenski <alacenski@gmail.com>
2016-09-26 16:43:10 -0700

    Created basic calculator.

--- CPTipCalculator/ViewController.swift
+++ CPTipCalculator/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let tipPercentages: [Double] = [0.18, 0.2, 0.25]
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTapScreen(sender: AnyObject) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(sender: AnyObject) {
+        
+        let billAmount: Double = Double(billField.text!) ?? 0
+        let tipPercent = tipPercentages[tipControl.selectedSegmentIndex]
 
+        let tipAmount = billAmount * tipPercent
+        
+        tipLabel.text = String(format: "$%.2f", tipAmount)
+        totalLabel.text = String(format: "$%.2f", billAmount + tipAmount)
+    }
 }
 
